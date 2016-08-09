3c963c69239f73741aacdacdeef1981598f0e935
Luiz SSB <luizssb.biz@gmail.com>
2016-08-09 09:16:28 -0300

    initial

---
+++ Assets/Unity Cluster Package/Scripts/Node/InstantiateNode.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace UnityClusterPackage {
+	
+	public class InstantiateNode : MonoBehaviour {
+		
+		public GameObject MultiProjectionCamera;
+		
+		void Awake() {
+			Network.sendRate = 100;
+
+			if ( NodeInformation.type.Equals("master") )
+			{
+				Network.proxyIP = NodeInformation.serverIp;
+				bool useNat = Network.HavePublicAddress();
+				Network.InitializeServer( NodeInformation.nodes, NodeInformation.serverPort, useNat );				
+				var networkedCamera = Network.Instantiate(
+					MultiProjectionCamera,
+					transform.position,
+					transform.rotation,
+					0
+				) as GameObject;
+				networkedCamera.transform.parent = this.transform;
+			}
+			else if ( NodeInformation.type.Equals("slave") )
+			{
+				Network.Connect( NodeInformation.serverIp, NodeInformation.serverPort );
+			}
+			
+		}
+		
+		void OnServerInitialized()
+		{
+			Debug.Log( "Server initialized and ready." );
+		}
+		
+		void OnPlayerConnected(NetworkPlayer player) {
+			Debug.Log( "Node connected " + player.ipAddress + ":" + player.port );
+		}
+		
+		void OnPlayerDisconnected( NetworkPlayer node ) {
+			Debug.Log( "Clean up after player " + node );
+			Network.RemoveRPCs(node);
+			Network.DestroyPlayerObjects(node);
+		}
+		
+		void OnConnectedToServer()
+		{
+			Debug.Log( "Connected to server." );
+		}
+		
+		void OnFailedToConnect(NetworkConnectionError error)
+		{
+			Debug.Log( "Could not connect to server: " + error );
+		}
+		
+		void OnDestroy() {
+			
+			if ( NodeInformation.type.Equals("master") )
+			{
+				Network.Disconnect();
+			}
+			else if ( NodeInformation.type.Equals("slave") )
+			{
+				Network.CloseConnection( Network.player, true );                
+			}
+		}
+		
+	}
+}
